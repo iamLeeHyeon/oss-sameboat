c8f05a6469c9e2738bd6defd6684edc60cb28eb0
iamLeeHyeon <107750736+iamLeeHyeon@users.noreply.github.com>
2022-12-05 23:30:13 +0900

    Update main.js
    날짜 기능 추가함요 하하

--- main.js
+++ main.js
@@ -52,3 +52,20 @@ function addText() {
     document.getElementById('img3').setAttribute('src', './imh/potato.png')
   }
 }
+function mydate()
+{
+  //alert("");
+document.getElementById("dt").hidden=false;
+document.getElementById("ndt").hidden=true;
+}
+function mydate1()
+{
+ d=new Date(document.getElementById("dt").value);
+dt=d.getDate();
+mn=d.getMonth();
+mn++;
+yy=d.getFullYear();
+document.getElementById("ndt").value=dt+"/"+mn+"/"+yy
+document.getElementById("ndt").hidden=false;
+document.getElementById("dt").hidden=true;
+}
