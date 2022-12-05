5369a1498d07e5f7fca819d6220c2d4217d29eb8
jangjihyee <107750387+jangjihyee@users.noreply.github.com>
2022-12-05 23:52:50 +0900

    Create excel.js
    데이터 입력 받으면 엑셀에 저장, 파일 생성

---
+++ excel.js
@@ -0,0 +1,52 @@
+var xlsx = require("xlsx");
+
+const data = inputData;
+function datenum(v, date1904) {
+    if (date1904) v += 1462;
+    const epoch = Date.parse(v);
+    return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
+
+function sheet_from_array_of_arrays(data, opts) {
+    const ws = {};
+    const range = { s: { c: 10000000, r: 10000000 }, e: { c: 0, r: 0 } };
+    for (let R = 0; R != data.length; ++R) {
+        for (let C = 0; C != data[R].length; ++C) {
+            if (range.s.r > R) range.s.r = R;
+            if (range.s.c > C) range.s.c = C;
+            if (range.e.r < R) range.e.r = R;
+            if (range.e.c < C) range.e.c = C;
+            const cell = { v: data[R][C] };
+            if (cell.v == null) continue;
+            const cell_ref = xlsx.utils.encode_cell({ c: C, r: R });
+
+            if (typeof cell.v === 'number') cell.t = 'n';
+            else if (typeof cell.v === 'boolean') cell.t = 'b';
+            else if (cell.v instanceof Date) {
+                cell.t = 'n';
+                cell.z = xlsx.SSF._table[14];
+                cell.v = datenum(cell.v);
+            } else cell.t = 's';
+
+            ws[cell_ref] = cell;
+        }
+    }
+    if (range.s.c < 10000000) ws['!ref'] = xlsx.utils.encode_range(range);
+    return ws;
+}
+
+const ws_name = "SheetJS";
+
+function Workbook() {
+    if (!(this instanceof Workbook)) return new Workbook();
+    this.SheetNames = [];
+    this.Sheets = {};
+}
+
+let wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+
+
+wb.SheetNames.push(ws_name);
+wb.Sheets[ws_name] = ws;
+
+xlsx.writeFile(wb, 'test.xlsx');
