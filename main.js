03c585dbd2d76b82fad4e26573c7e3cd452d2dd5
ohjaesik <ojaesig86@gmail.com>
2022-12-07 10:20:17 +0900

    엑셀 저장 및 불러오기 추가하기 기능 수정

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 const { json } = require("express");
 
-function readExcel() {
-  let input = event.target;
-  let reader = new FileReader();
+function readExcel(event){
+	excelExportCommon(event, handleExcelDataAll);
+}
+function excelExportCommon(event, callback){
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function(){
+        var fileData = reader.result;
+        var wb = XLSX.read(fileData, {type : 'binary'});
+        var sheetNameList = wb.SheetNames; // 시트 이름 목록 가져오기 
+        var firstSheetName = sheetNameList[0]; // 첫번째 시트명
+        var firstSheet = wb.Sheets[firstSheetName]; // 첫번째 시트 
+        callback(firstSheet);      
+    };
+    reader.readAsBinaryString(input.files[0]);
+}
+function handleExcelDataAll(sheet){
+	handleExcelDataHtml(sheet); // html 형태
+}
+function handleExcelDataHtml(sheet){
+    $("#displayExcelHtml").html(XLSX.utils.sheet_to_html (sheet));
+}
 
-  reader.onload = function() {
-    let data = reader.result;
-    let workBook = XLSX.read(data, {type:'binary'});
+// 출처 : https://github.com/SheetJS/js-xlsx/issues/214
+function get_header_row(sheet) {
+    var headers = [];
+    var range = XLSX.utils.decode_range(sheet['!ref']);
+    var C, R = range.s.r; /* start in the first row */
+    /* walk every column in the range */
+    for(C = range.s.c; C <= range.e.c; ++C) {
+        var cell = sheet[XLSX.utils.encode_cell({c:C, r:R})] /* find the cell in the first row */
 
-    workBook.SheetNames.forEach(function(sheetName){
-      console.log('SheetName: ' + sheetName);
+        var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+        if(cell && cell.t) hdr = XLSX.utils.format_cell(cell);
 
-      let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
-        document.getElementById("content").innerHTML = JSON.stringify(rows)
-    })
-  };
-
-  reader.readAsBinaryString(input.files[0]);
+        headers.push(hdr);
+    }
+    return headers;
 }
 
 function saveExcel() {
+  var table = document.querySelector('table');
+  table.setAttribute('id', 'test');
   let wb = XLSX.utils.book_new();
   let ws = XLSX.utils.table_to_sheet(document.getElementById("test"));
   XLSX.utils.book_append_sheet(wb, ws, "sheet title");
@@ -27,12 +50,20 @@ function saveExcel() {
 }
   const idx = 1;
 function addText() {
+  const tr = document.querySelectorAll("tr");
+  for(var i=0; i<tr.length; i++) {
+    tr[i].setAttribute('id', 'tr');
+  }
+  const tbody = document.querySelector('tbody');
+  const newtr = document.createElement("tr");
+  newtr.setAttribute('id','newtr');
+  tbody.appendChild(newtr);
   const name = document.getElementById('add_text').value;
   const td = document.createElement("td");
   td.setAttribute('id',name);
   const textNode = document.createTextNode(name);
   td.appendChild(textNode);
-  document.getElementById("addTable").appendChild(td);
+  document.getElementById('newtr').appendChild(td);
   if(name === '오이') {
     const img = document.createElement("img");
     img.setAttribute('id','img1');
